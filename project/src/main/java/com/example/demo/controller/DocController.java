6743509db5026cd785e8d5a5f9ed9fb7c9dc19b3
Mikyung Jeon <mikyung22e@naver.com>
2018-12-03 18:25:19 +0900

    181203 doc add

--- project/src/main/java/com/example/demo/controller/DocController.java
+++ project/src/main/java/com/example/demo/controller/DocController.java
@@ -3,14 +3,18 @@ package com.example.demo.controller;
 import static com.example.demo.controller.Util.response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dao.DocDao;
+import com.example.demo.domain.Doc;
 
 @CrossOrigin("*")
 @RestController
@@ -24,8 +28,23 @@ public class DocController {
 		return response(dao.findAll());
 	}
 	
+	@GetMapping("/{id}")
+	public Object findByDoc(@PathVariable int id) {
+		return response(dao.findOne(id));
+	}
+	
 	@PostMapping("/u")
 	public Object findByUpinfo(@RequestBody int deptId) {
 		return response(dao.findByUpinfo(deptId));
+	}	
+	
+	@PostMapping
+	public Object insert(@RequestBody Doc doc) {
+		return response(dao.insert(doc), HttpStatus.FOUND);
+	}
+	
+	@PutMapping
+	public Object update(@RequestBody Doc doc) {
+		return response(dao.update(doc), HttpStatus.CONFLICT);
 	}	
 }
--- project/src/main/java/com/example/demo/dao/DocDao.java
+++ project/src/main/java/com/example/demo/dao/DocDao.java
@@ -2,7 +2,9 @@ package com.example.demo.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.SelectKey;
 
 import com.example.demo.domain.Doc;
 import com.example.demo.domain.DocAppr;
@@ -12,5 +14,13 @@ public interface DocDao {
 
 	public List<Doc> findAll();
 	public DocAppr findByUpinfo(int deptId);
+	
+	@Insert("insert into doc(title ,body ,publish ,author ,ts) values(#{title}, #{body}, #{publish}, #{author}, #{ts})")
+	@SelectKey(statement="select LAST_INSERT_ID()", before=false, keyProperty="id", resultType=Integer.class)
+	public int insert(Doc doc);
+	
+	public Doc findOne(int id);
+			
+	public int update(Doc doc);
 
 }
